2da52f6706dc51a96b713c3772efacb9e54297ab
yvonne <yvonnehuang0810@gmail.com>
2024-08-02 23:41:11 -0400

    changing tags

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import Home from './Home.js'
-import Header from "./Header.js"
+import Home from './Home.jsx'
+import Header from "./Header.jsx"
 
 import reportWebVitals from './reportWebVitals.js';
 

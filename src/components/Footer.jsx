01b0adbfe38cd7683b4b03bdf7ccf691c78ce354
yvonne <yvonnehuang0810@gmail.com>
2024-08-04 11:31:39 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,14 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer-container">
-                <p>
-                    test
-                </p>
+                <footer>
+                    <nav id="nav-bar">
+                        <a href="#home-page" id="footer-nav">Home</a>
+                        <a href="#about-page" id="footer-nav">About</a>
+                        <a href="#contact-page" id="footer-nav">Contact</a>
+                    </nav>
+                </footer>
+                <p id="copyright">© Crewmeleon, 2024.</p>
             </div>
         </div>
     )
